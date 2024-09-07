7e0181ea511db9907c9d9308302966a4eab43b51
hatchiman21 <111010009+hatchiman21@users.noreply.github.com>
2024-09-07 05:41:03 +0300

    Update ft_printf_test.c

--- ft_printf_test.c
+++ ft_printf_test.c
@@ -186,10 +186,13 @@ static unsigned long int	allocation_hex(unsigned long int n, char **res, int fd)
 		n /= 16;
 		i++;
 	}
-	if (fd != 3)
-		*res = (char *)malloc(sizeof(char) * (i + 1));
+	if (fd == 3)
+	{
+		*res = (char *)malloc(sizeof(char) * (i + 2));
+		return (i + 1);
+	}
 	else
-		*res = (char *)malloc(sizeof(char) * (i + 3));
+		*res = (char *)malloc(sizeof(char) * i);
 	return (i - 1);
 }
 
@@ -199,7 +202,7 @@ static char	*ft_itoa_hex_step(unsigned long int m,int fd, int i, char *res)
 	{
 		if ((m % 16) < 10)
 			res[i--] = (m % 16) + '0';
-		else if (fd == 1)
+		else if (fd == 1 || fd == 3)
 			res[i--] = (m % 16) - 10 + 'a';
 		else if (fd == 2)
 			res[i--] = (m % 16) - 10 + 'A';
